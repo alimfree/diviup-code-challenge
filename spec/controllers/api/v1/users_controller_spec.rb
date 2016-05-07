d618ed998439329037c5837cdb3943cf87e879a8
Ali <m.ali.overthrow@gmail.com>
2016-05-07 12:23:14 -0500

    adding test for creating users

--- spec/controllers/api/v1/users_controller_spec.rb
+++ spec/controllers/api/v1/users_controller_spec.rb
@@ -18,4 +18,38 @@ describe Api::V1::UsersController do
 
 	it { should respond_with 200 }
   end
+
+  describe "POST #create" do
+    context "when successfully crreated" do
+	  before(:each) do
+	    @user_attributes = Factorygirl.attributes_for :user
+		post :create, { user: @user_attributes }, format: :json
+	  end
+
+      it "renders newly created user as json" do
+	    user_response = JSON.parse(response.body, symbolize_names: true)
+		expect(user_response[:email]).to eql @user_attributes[:email]
+	  end
+
+	  it { should respond_with 201 }
+    end
+	  
+	context "when creation fails" do
+	  before(:each) do
+	    # purposfully omitting required email
+	    @invalid_user_attributes = { password: "12345678", password_confirmation: "12345678" } 
+		post :create, { user: @invalid_user_attributes }, format: :json
+	  end
+
+	  it "renders an error" do
+	    user_response = JSON.parse(response.body, symbolize_names: true)
+		expect(user_response).to have_key(:errors)
+      end
+
+	  it "properly displays reason why user was not created" do
+	    user_response = JSON.parse(response.body, symbolize_names: true)
+		expect(user_response[:errors][:email]).to include "can't be blank"
+	  end
+	end
+  end
 end
