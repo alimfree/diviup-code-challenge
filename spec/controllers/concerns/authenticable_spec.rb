ea55e5d800f2989b936a5b309b45a2ce36bc09d8
Ali <m.ali.overthrow@gmail.com>
2016-05-07 16:26:47 -0500

    Added user_signed_in? method to keep track of user sessions or logged in status

--- spec/controllers/concerns/authenticable_spec.rb
+++ spec/controllers/concerns/authenticable_spec.rb
@@ -5,33 +5,53 @@ class Authentication
 end
 
 describe Authenticable do
-  let(:authentication) { Authentication.new }
+  let(:authentication) { Authentication.new } 
   subject { authentication }
 
   describe "#current_user" do
     before do
-	  @user = FactoryGirl.create :user
-	  request.headers["Authorization"] = @user.auth_token
-	  authentication.stub(:request).and_return(request)
-	end
-	it "fetchs user by the authorization token" do
-	  expect(authentication.current_user.auth_token).to eql @user.auth_token
-	end
+      @user = FactoryGirl.create :user
+      request.headers["Authorization"] = @user.auth_token
+      authentication.stub(:request).and_return(request)
+    end
+    it "retrieves current user from authorization header" do
+      expect(authentication.current_user.auth_token).to eql @user.auth_token
+    end
   end
 
   describe "#authenticate_with_token" do
     before do
-	  @user = FactoryGirl.create :user
-	  authentication.stub(:current_user).and_return(nil)
-	  response.stub(:response_code).and_return(401)
-	  response.stub(:body).and_return({"errors" => "Not authenticated"}.to_json)
-	  authentication.stub(:response).and_return(response)
+      @user = FactoryGirl.create :user
+      authentication.stub(:current_user).and_return(nil)
+      response.stub(:response_code).and_return(401)
+      response.stub(:body).and_return({"errors" => "Not authenticated"}.to_json)
+      authentication.stub(:response).and_return(response)
     end
 
-    it "display a json error" do
-	  expect(json_response[:errors]).to eql "Not authenticated"
+    it "displays a json error" do
+      expect(json_response[:errors]).to eql "Not authenticated"
     end
 
-	it {  should respond_with 401 }
+    it {  should respond_with 401 }
+  end
+
+  describe "#user_signed_in?" do
+    context "successfully authenticates user and creates a session" do
+      before do
+        @user = FactoryGirl.create :user
+        authentication.stub(:current_user).and_return(@user)
+      end
+
+      it { should be_user_signed_in }
+    end
+
+    context "Fails to authenticate user and start a session" do
+      before do
+        @user = FactoryGirl.create :user
+        authentication.stub(:current_user).and_return(nil)
+      end
+
+      it { should_not be_user_signed_in }
+    end
   end
 end
