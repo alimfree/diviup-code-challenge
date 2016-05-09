495e0c4ae1728dd4e9fb4f9fcc16f42c2d4d7053
Ali <m.ali.overthrow@gmail.com>
2016-05-08 21:42:57 -0500

    added seed file to populate database

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,6 @@
+require 'rubygems' 
+require 'factory_girl_rails'
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
@@ -5,3 +8,9 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+# Create sample data 
+
+4.times { FactoryGirl.create :user }
+20.times { FactoryGirl.create :list }
